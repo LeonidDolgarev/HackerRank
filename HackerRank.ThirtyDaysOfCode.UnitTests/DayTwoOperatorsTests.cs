c00173f243bc53705de24877da7747d27ce47d8c
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-11-23 16:35:40 +0200

    30 Days of Code: Day 2: Operators

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayTwoOperatorsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayTwoOperatorsTests
+    {
+        [Theory]
+        [InlineData("12,00\r\n20\r\n8", "15\r\n")]
+        [InlineData("15,50\r\n15\r\n10", "19\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayTwoOperators.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayTwoOperators.cs
@@ -0,0 +1,40 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayTwoOperators
+    {
+        private sealed class Result
+        {
+            /*
+             * Complete the 'solve' function below.
+             *
+             * The function accepts following parameters:
+             *  1. DOUBLE meal_cost
+             *  2. INTEGER tip_percent
+             *  3. INTEGER tax_percent
+             */
+
+            public static void Solve(double meal_cost, int tip_percent, int tax_percent)
+            {
+                double tip = tip_percent * meal_cost / 100;
+                double tax = meal_cost * tax_percent / 100;
+                double total_cost = meal_cost + tip + tax;
+
+                Console.WriteLine(Math.Round(total_cost));
+            }
+        }
+
+        public sealed class Solution
+        {
+            public static void Main(string[] args)
+            {
+                double meal_cost = Convert.ToDouble(Console.ReadLine().Trim());
+
+                int tip_percent = Convert.ToInt32(Console.ReadLine().Trim());
+
+                int tax_percent = Convert.ToInt32(Console.ReadLine().Trim());
+
+                Result.Solve(meal_cost, tip_percent, tax_percent);
+            }
+        }
+    }
+}
