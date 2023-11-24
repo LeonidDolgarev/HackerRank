c959d0bf1c8bb45bd8cab20279e536cf7ffb0148
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-11-24 10:25:31 +0200

    30 Days of Code: Day 3: Intro to Conditional Statements

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayThreeIntroToConditionalStatementsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayThreeIntroToConditionalStatementsTests
+    {
+        [Theory]
+        [InlineData("3", "Weird\r\n")]
+        [InlineData("24", "Not Weird\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayThreeIntroToConditionalStatements.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayThreeIntroToConditionalStatements.cs
@@ -0,0 +1,31 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayThreeIntroToConditionalStatements
+    {
+        public sealed class Solution
+        {
+            public static void Main(string[] args)
+            {
+                int N = Convert.ToInt32(Console.ReadLine().Trim());
+                var isEven = (int n) => n % 2 == 0;
+
+                /*
+                If  is odd, print Weird
+                If  is even and in the inclusive range of 2 to 5, print Not Weird
+                If  is even and in the inclusive range of 6 to 20, print Weird
+                If  is even and greater than 20, print Not Weird
+                */
+                string? result = isEven(N) switch
+                {
+                    false => "Weird",
+                    true when N >= 2 && N <= 5 => "Not Weird",
+                    true when N >= 6 && N <= 20 => "Weird",
+                    true when N > 20 => "Not Weird",
+                    _ => null,
+                };
+
+                if (result is not null) Console.WriteLine(result);
+            }
+        }
+    }
+}
