07464c4613fd3fa68bbf4b437855af27a54a50d9
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-11-26 01:05:52 +0200

    30 Days of Code: Day 4: Class vs. Instance

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayFourClassVsInstanceTests.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayFourClassVsInstanceTests
+    {
+        [Theory]
+        [InlineData("4\r\n-1\r\n10\r\n16\r\n18", "Age is not valid, setting age to 0.\r\nYou are young.\r\nYou are young.\r\n\r\nYou are young.\r\nYou are a teenager.\r\n\r\nYou are a teenager.\r\nYou are old.\r\n\r\nYou are old.\r\nYou are old.\r\n\r\n")]
+        public void Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayFourClassVsInstance.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayFourClassVsInstance.cs
@@ -0,0 +1,55 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayFourClassVsInstance
+    {
+        private class Person
+        {
+            public int age;
+            public Person(int initialAge)
+            {
+                // Add some more code to run some checks on initialAge
+                if (initialAge < 0)
+                {
+                    Console.WriteLine("Age is not valid, setting age to 0.");
+                    return;
+                }
+                age = initialAge;
+            }
+            public void amIOld()
+            {
+                // Do some computations in here and print out the correct statement to the console
+                string result = age switch
+                {
+                    < 13 => "You are young.",
+                    >= 13 and < 18 => "You are a teenager.",
+                    _ => "You are old."
+                };
+
+                Console.WriteLine(result);
+            }
+
+            public void yearPasses()
+            {
+                // Increment the age of the person in here
+                age++;
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            int T = int.Parse(Console.In.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int age = int.Parse(Console.In.ReadLine());
+                Person p = new Person(age);
+                p.amIOld();
+                for (int j = 0; j < 3; j++)
+                {
+                    p.yearPasses();
+                }
+                p.amIOld();
+                Console.WriteLine();
+            }
+        }
+    }
+}
