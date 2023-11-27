aa4a99fc8fc6eebad176ceb317d631a225a9d690
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-11-27 06:33:11 +0200

    30 Days of Code: Day 5: Loops

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayFiveLoopsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayFiveLoopsTests
+    {
+        [Theory]
+        [InlineData("2", "2 x 1 = 2\r\n2 x 2 = 4\r\n2 x 3 = 6\r\n2 x 4 = 8\r\n2 x 5 = 10\r\n2 x 6 = 12\r\n2 x 7 = 14\r\n2 x 8 = 16\r\n2 x 9 = 18\r\n2 x 10 = 20\r\n")]
+        [InlineData("3", "3 x 1 = 3\r\n3 x 2 = 6\r\n3 x 3 = 9\r\n3 x 4 = 12\r\n3 x 5 = 15\r\n3 x 6 = 18\r\n3 x 7 = 21\r\n3 x 8 = 24\r\n3 x 9 = 27\r\n3 x 10 = 30\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValue(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayFiveLoops.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayFiveLoops.cs
@@ -0,0 +1,18 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayFiveLoops
+    {
+        public sealed class Solution
+        {
+            public static void Main(string[] args)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{n} x {i} = {n * i}");
+                }
+            }
+        }
+    }
+}
