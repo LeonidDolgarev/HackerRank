91253bd00daa9e1fbae7ba5cb9d5169aedfaa43b
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-11-28 21:46:52 +0200

    30 Days of Code: Day 7: Arrays

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DaySevenArraysTests.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DaySevenArraysTests
+    {
+        [Theory]
+        [InlineData("4\r\n1 4 3 2", "2 3 4 1\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValue(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DaySevenArrays.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DaySevenArrays.cs
@@ -0,0 +1,20 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DaySevenArrays
+    {
+        public sealed class Solution
+        {
+            public static void Main(string[] args)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+                List<int> reversed = new(arr);
+                reversed.Reverse();
+
+                Console.WriteLine(string.Join(" ", reversed));
+            }
+        }
+    }
+}
