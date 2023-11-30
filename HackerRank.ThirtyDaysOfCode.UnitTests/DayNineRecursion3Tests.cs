b8e0d4ab1bae3454120753014da7c32e9c4da4a1
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-11-30 16:00:52 +0200

    30 Days of Code: Day 9: Recursion 3

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayNineRecursion3Tests.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayNineRecursion3Tests
+    {
+        [Theory]
+        [InlineData("3", "6\r\n")]
+        [InlineData("11", "39916800\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValue(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayNineRecursion3.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayNineRecursion3.cs
@@ -0,0 +1,34 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayNineRecursion3
+    {
+        public sealed class Result
+        {
+            /*
+             * Complete the 'factorial' function below.
+             *
+             * The function is expected to return an INTEGER.
+             * The function accepts INTEGER n as parameter.
+             */
+
+            public static int factorial(int n)
+            {
+                if (n <= 1) return 1;
+                return n * factorial(n - 1);
+            }
+
+        }
+
+        public sealed class Solution
+        {
+            public static void Main(string[] args)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                int result = Result.factorial(n);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
