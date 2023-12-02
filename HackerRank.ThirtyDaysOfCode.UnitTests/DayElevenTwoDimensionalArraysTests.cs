3f31bf374067199778475c5c6060e45d43448b4b
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-02 20:46:50 +0200

    30 Days of Code: Day 11: 2D Arrays

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayElevenTwoDimensionalArraysTests.cs
@@ -0,0 +1,27 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayElevenTwoDimensionalArraysTests
+    {
+        [Theory]
+        [InlineData("1 1 1 0 0 0\r\n0 1 0 0 0 0\r\n1 1 1 0 0 0\r\n0 0 0 0 0 0\r\n0 0 0 0 0 0\r\n0 0 0 0 0 0", "7\r\n")]
+        [InlineData("1 1 1 0 0 0\r\n0 1 0 0 0 0\r\n1 1 1 0 0 0\r\n0 0 2 4 4 0\r\n0 0 0 2 0 0\r\n0 0 1 2 4 0", "19\r\n")]
+        [InlineData("-1 -1 0 -9 -2 -2\r\n-2 -1 -6 -8 -2 -5\r\n-1 -1 -1 -2 -3 -4\r\n-1 -9 -2 -4 -4 -5\r\n-7 -3 -3 -2 -9 -9\r\n-1 -3 -1 -2 -4 -5", "-6\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayElevenTwoDimensionalArrays.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayElevenTwoDimensionalArrays.cs
@@ -0,0 +1,35 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayElevenTwoDimensionalArrays
+    {
+        public sealed class Solution
+        {
+            public static void Main(string[] args)
+            {
+                List<List<int>> arr = new List<List<int>>();
+
+                for (int i = 0; i < 6; i++)
+                {
+                    arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+                }
+
+                int max = int.MinValue;
+                int n = arr.Count - 3 + 1;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        max = Math.Max(max, hourglassSum(ref arr, i, j));
+                    }
+                }
+
+                Console.WriteLine(max);
+
+                int hourglassSum(ref List<List<int>> arr, int i, int j) =>
+                    arr[i][j] + arr[i][j + 1] + +arr[i][j + 2]
+                    + arr[i + 1][j + 1]
+                    + arr[i + 2][j] + arr[i + 2][j + 1] + +arr[i + 2][j + 2];
+            }
+        }
+    }
+}
