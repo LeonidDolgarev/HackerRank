e97e03629294ef28468bf8a5f894fec30ae9ef55
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-04 18:00:13 +0200

    30 Days of Code: Day 13: Abstract Classes

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayThirteenAbstractClassesTests.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayThirteenAbstractClassesTests
+    {
+        [Theory]
+        [InlineData("The Alchemist\r\nPaulo Coelho\r\n248", "Title: The Alchemist\r\nAuthor: Paulo Coelho\r\nPrice: 248\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayThirteenAbstractClasses.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayThirteenAbstractClasses.cs
@@ -0,0 +1,49 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayThirteenAbstractClasses
+    {
+        private abstract class Book
+        {
+            protected String title;
+            protected String author;
+
+            public Book(String t, String a)
+            {
+                title = t;
+                author = a;
+            }
+            public abstract void display();
+        }
+
+        //Write MyBook class
+        private sealed class MyBook : Book
+        {
+            private readonly int _price;
+
+            internal MyBook(in String title, in String author, int price)
+                : base(title, author)
+            {
+                _price = price;
+            }
+
+            public override void display()
+            {
+                Console.WriteLine($"Title: {title}");
+                Console.WriteLine($"Author: {author}");
+                Console.WriteLine($"Price: {_price}");
+            }
+        }
+
+        public sealed class Solution
+        {
+            public static void Main(String[] args)
+            {
+                String title = Console.ReadLine();
+                String author = Console.ReadLine();
+                int price = Int32.Parse(Console.ReadLine());
+                Book new_novel = new MyBook(title, author, price);
+                new_novel.display();
+            }
+        }
+    }
+}
