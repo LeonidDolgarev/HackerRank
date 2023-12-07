206311caf7e037c79aed77a1db571dd4841b0396
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-07 14:02:23 +0200

    30 Days of Code: Day 14: Scope

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayFourteenScopeTests.cs
@@ -0,0 +1,27 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayFourteenScopeTests
+    {
+        [Theory]
+        //[InlineData("3\r\n1 2 5", "4")]
+        [InlineData("5\r\n8 19 3 2 7", "17")]
+        //[InlineData("5\r\n8 8 8 8 8", "0")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayFourteenScope.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayForteenScope.cs
@@ -0,0 +1,47 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayFourteenScope
+    {
+        private sealed class Difference
+        {
+            private int[] elements;
+            public int maximumDifference;
+
+            // Add your code here
+            public Difference(int[] a)
+            {
+                elements = a;
+            }
+
+            public void computeDifference()
+            {
+                int minElement = int.MaxValue;
+                int maxElement = int.MinValue;
+
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    minElement = Math.Min(elements[i], minElement);
+                    maxElement = Math.Max(elements[i], maxElement);
+                }
+
+                maximumDifference = maxElement - minElement;
+            }
+        }
+
+        public sealed class Solution
+        {
+            public static void Main(string[] args)
+            {
+                Convert.ToInt32(Console.ReadLine());
+
+                int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+                Difference d = new Difference(a);
+
+                d.computeDifference();
+
+                Console.Write(d.maximumDifference);
+            }
+        }
+    }
+}
