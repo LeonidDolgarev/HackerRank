c48fc609a3d655ca27c1da723b2fa21a11fa9cf2
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-07 15:06:32 +0200

    30 Days of Code: Day 16: Exceptions - String to Integer

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DaySixteenExceptionsStringToIntegerTests.cs
@@ -0,0 +1,28 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DaySixteenExceptionsStringToIntegerTests
+    {
+        [Theory]
+        [InlineData("3", "3")]
+        [InlineData("za", "Bad String")]
+        [InlineData("3134", "3134")]
+        [InlineData("abc", "Bad String")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValue(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DaySixteenExceptionsStringToInteger.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DaySixteenExceptionsStringToInteger.cs
@@ -0,0 +1,23 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DaySixteenExceptionsStringToInteger
+    {
+        public sealed class Solution
+        {
+            public static void Main(string[] args)
+            {
+                string S = Console.ReadLine();
+
+                try
+                {
+                    int n = Int32.Parse(S);
+                    Console.Write(n);
+                }
+                catch
+                {
+                    Console.Write("Bad String");
+                }
+            }
+        }
+    }
+}
