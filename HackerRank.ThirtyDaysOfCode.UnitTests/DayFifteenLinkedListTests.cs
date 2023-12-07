697ee1dc30cbaad139d461f75d6b94740f4b53c2
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-07 15:06:08 +0200

    30 Days of Code: Day 15: Linked List

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayFifteenLinkedListTests.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayFifteenLinkedListTests
+    {
+        [Theory]
+        [InlineData("4\r\n2\r\n3\r\n4\r\n1", "2 3 4 1 ")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayFifteenLinkedList.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayFifteenLinkedList.cs
@@ -0,0 +1,56 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayFifteenLinkedList
+    {
+        public sealed class Node
+        {
+            public int data;
+            public Node? next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        public sealed class Solution
+        {
+            public static Node? insert(Node? head, int data)
+            {
+                //Complete this method
+                var node = new Node(data);
+                if (head is null) return node;
+
+                var tail = head;
+                while (tail.next is not null)
+                {
+                    tail = tail.next;
+                }
+
+                tail.next = node;
+
+                return head;
+            }
+
+            public static void display(Node head)
+            {
+                Node? start = head;
+                while (start != null)
+                {
+                    Console.Write(start.data + " ");
+                    start = start.next;
+                }
+            }
+            public static void Main(String[] args)
+            {
+                Node? head = null;
+                int T = Int32.Parse(Console.ReadLine());
+                while (T-- > 0)
+                {
+                    int data = Int32.Parse(Console.ReadLine());
+                    head = insert(head, data);
+                }
+                display(head);
+            }
+        }
+    }
+}
