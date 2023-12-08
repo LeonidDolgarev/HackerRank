b6f3ece63395eaa4e2f93e21083b45c486a389ed
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-08 12:46:20 +0200

    30 Days of Code: Day 17: More Exceptions

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DaySeventeenMoreExceptionsTests.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DaySeventeenMoreExceptionsTests
+    {
+        [Theory]
+        [InlineData("4\r\n3 5\r\n2 4\r\n-1 -2\r\n-1 3", "243\r\n16\r\nn and p should be non-negative\r\nn and p should be non-negative\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DaySeventeenMoreExceptions.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DaySeventeenMoreExceptions.cs
@@ -0,0 +1,50 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DaySeventeenMoreExceptions
+    {
+        private sealed class Calculator
+        {
+            internal int power(int n, int p)
+            {
+                if (n < 0 || p < 0)
+                    throw new ArgumentException("n and p should be non-negative");
+
+                if (p == 0) return 1;
+
+                int r = 1;
+                while (p > 0)
+                {
+                    r *= n;
+                    p--;
+                }
+
+                return r;
+            }
+        }
+
+        public sealed class Solution
+        {
+            public static void Main(String[] args)
+            {
+                Calculator myCalculator = new Calculator();
+                int T = Int32.Parse(Console.ReadLine());
+                while (T-- > 0)
+                {
+                    string[] num = Console.ReadLine().Split();
+                    int n = int.Parse(num[0]);
+                    int p = int.Parse(num[1]);
+                    try
+                    {
+                        int ans = myCalculator.power(n, p);
+                        Console.WriteLine(ans);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+
+                    }
+                }
+            }
+        }
+    }
+}
