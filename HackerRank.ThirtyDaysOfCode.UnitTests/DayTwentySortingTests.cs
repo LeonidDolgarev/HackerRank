ee6c0d1b747745571d47a612f1586556fb3429d0
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-13 22:52:58 +0200

    30 Days of Code: Day 20: Sorting

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayTwentySortingTests.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayTwentySortingTests
+    {
+        [Theory]
+        [InlineData("3\r\n1 2 3", "Array is sorted in 0 swaps.\r\nFirst Element: 1\r\nLast Element: 3\r\n")]
+        [InlineData("3\r\n3 2 1", "Array is sorted in 3 swaps.\r\nFirst Element: 1\r\nLast Element: 3\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayTwentySorting.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayTwentySorting.cs
@@ -0,0 +1,52 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayTwentySorting
+    {
+        public sealed class Solution
+        {
+            public static void Main(string[] args)
+            {
+                int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+                List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+                // Write your code here
+                int numberOfSwaps = BubbleSort(a, a.Count);
+
+                Console.WriteLine($"Array is sorted in {numberOfSwaps} swaps.");
+                Console.WriteLine($"First Element: {a.First()}");
+                Console.WriteLine($"Last Element: {a.Last()}");
+            }
+
+            private static int BubbleSort(in List<int> a, int n)
+            {
+                // Track number of elements swapped during a single array traversal
+                int numberOfSwaps = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n - 1; j++)
+                    {
+                        // Swap adjacent elements if they are in decreasing order
+                        if (a[j] > a[j + 1])
+                        {
+                            swap(a, j, j + 1);
+                            numberOfSwaps++;
+                        }
+                    }
+
+                    // If no elements were swapped during a traversal, array is sorted
+                    if (numberOfSwaps == 0)
+                    {
+                        break;
+                    }
+                }
+
+                return numberOfSwaps;
+            }
+
+            private static void swap<T>(List<T> list, int i, int j) =>
+                (list[i], list[j]) = (list[j], list[i]);
+        }
+    }
+}
