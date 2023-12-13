b761118a5f74091413301ca2c446670b602e892c
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-13 16:56:32 +0200

    30 Days of Code: Day 18: Queues and Stacks

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayEighteenQueuesAndStacksTests.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayEighteenQueuesAndStacksTests
+    {
+        [Theory]
+        [InlineData("racecar", "The word, racecar, is a palindrome.")]
+        [InlineData("yes", "The word, yes, is not a palindrome.")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValue(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayEighteenQueuesAndStacks.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
