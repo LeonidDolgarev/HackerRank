56ef08fe83f3ef3291d041a9b605f203d5b90df9
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-13 17:30:51 +0200

    30 Days of Code: Day 19: Interfaces

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayNineteenInterfacesTests.cs
@@ -0,0 +1,27 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayNineteenInterfacesTests
+    {
+        [Theory]
+        [InlineData("6", "I implemented: AdvancedArithmetic\r\n12\r\n")]
+        [InlineData("1", "I implemented: AdvancedArithmetic\r\n1\r\n")]
+        [InlineData("20", "I implemented: AdvancedArithmetic\r\n42\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValue(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayNineteenInterfaces.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayNineteenInterfaces.cs
@@ -0,0 +1,36 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayNineteenInterfaces
+    {
+        public interface AdvancedArithmetic
+        {
+            int divisorSum(int n);
+        }
+
+        public sealed class Calculator : AdvancedArithmetic
+        {
+            public int divisorSum(int n)
+            {
+                int sum = 0;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    if (n % i == 0) sum += i;
+                }
+
+                return sum;
+            }
+        }
+
+        public sealed class Solution
+        {
+            public static void Main(string[] args)
+            {
+                int n = Int32.Parse(Console.ReadLine());
+                AdvancedArithmetic myCalculator = new Calculator();
+                int sum = myCalculator.divisorSum(n);
+                Console.WriteLine("I implemented: AdvancedArithmetic\r\n" + sum);
+            }
+        }
+    }
+}
