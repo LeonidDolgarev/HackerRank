507e811cb3a89404725228134ba1c6f29a18d1bc
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-14 00:10:47 +0200

    30 Days of Code: Day 22: Binary Search Trees

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayTwentyTwoBinarySearchTreesTests.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayTwentyTwoBinarySearchTreesTests
+    {
+        [Theory]
+        [InlineData("7\r\n3\r\n5\r\n2\r\n1\r\n4\r\n6\r\n7", "3\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayTwentyTwoBinarySearchTrees.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayTwentyTwoBinarySearchTrees.cs
@@ -0,0 +1,66 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayTwentyTwoBinarySearchTrees
+    {
+        private sealed class Node
+        {
+            public Node? left, right;
+            public int data;
+
+            public Node(int data)
+            {
+                this.data = data;
+                left = right = null;
+            }
+        }
+        public sealed class Solution
+        {
+            private static int getHeight(in Node? root)
+            {
+                //Write your code here
+                if (root is null) return -1;
+
+                int leftHeight = getHeight(root.left);
+                int rightHeight = getHeight(root.right);
+
+                return Math.Max(leftHeight, rightHeight) + 1;
+            }
+
+            private static Node insert(Node? root, int data)
+            {
+                if (root == null)
+                {
+                    return new Node(data);
+                }
+                else
+                {
+                    Node cur;
+                    if (data <= root.data)
+                    {
+                        cur = insert(root.left, data);
+                        root.left = cur;
+                    }
+                    else
+                    {
+                        cur = insert(root.right, data);
+                        root.right = cur;
+                    }
+                    return root;
+                }
+            }
+
+            public static void Main(String[] args)
+            {
+                Node? root = null;
+                int T = Int32.Parse(Console.ReadLine());
+                while (T-- > 0)
+                {
+                    int data = Int32.Parse(Console.ReadLine());
+                    root = insert(root, data);
+                }
+                int height = getHeight(root);
+                Console.WriteLine(height);
+            }
+        }
+    }
+}
