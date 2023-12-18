d061dd5f194d27c3f2200277bcace60c0742f98d
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-18 08:21:59 +0200

    30 Days of Code: Day 26: Nested Logic

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayTwentySixNestedLogicTests.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayTwentySixNestedLogicTests
+    {
+        [Theory]
+        [InlineData("9 6 2015\r\n6 6 2015", "45")]
+        [InlineData("8 4 12\r\n1 1 1", "10000")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayTwentySixNestedLogic.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayTwentySixNestedLogic.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayTwentySixNestedLogic
+    {
+        public sealed class Solution
+        {
+            public static void Main(String[] args)
+            {
+                /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+                string[] formats = { "d M yyyy", "d M yy", "d M y" };
+                DateTime returnedDate = DateTime.ParseExact(Console.ReadLine(), formats, CultureInfo.InvariantCulture);
+                DateTime dueDate = DateTime.ParseExact(Console.ReadLine(), formats, CultureInfo.InvariantCulture);
+                int fine = calculateFine(returnedDate, dueDate);
+
+                Console.Write(fine);
+
+                int calculateFine(in DateTime returnedDate, in DateTime dueDate)
+                {
+                    if (dueDate > returnedDate)
+                        return 0;
+
+                    if (returnedDate.Month == dueDate.Month)
+                        return (returnedDate.Day - dueDate.Day) * 15;
+
+                    if (returnedDate.Year == dueDate.Year)
+                        return (returnedDate.Month - dueDate.Month) * 500;
+
+                    return 10000;
+                }
+            }
+        }
+    }
+}
