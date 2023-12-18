4347f8f236f45b197bc65d1bf75c511e24f3493c
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-18 08:20:04 +0200

    30 Days of Code: Day 24: More Linked Lists

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayTwentyFourMoreLinkedListsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayTwentyFourMoreLinkedListsTests
+    {
+        [Theory]
+        [InlineData("6\r\n1\r\n2\r\n2\r\n3\r\n3\r\n4", "1 2 3 4 ")]
+        [InlineData("20\r\n3\r\n9\r\n9\r\n11\r\n11\r\n11\r\n11\r\n89\r\n89\r\n100\r\n100\r\n101\r\n102\r\n103\r\n108\r\n200\r\n250\r\n250\r\n250\r\n250", "3 9 11 89 100 101 102 103 108 200 250 ")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayTwentyFourMoreLinkedLists.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayTwentyFourMoreLinkedLists.cs
@@ -0,0 +1,79 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayTwentyFourMoreLinkedLists
+    {
+        private sealed class Node
+        {
+            public int data;
+            public Node? next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+
+        public sealed class Solution
+        {
+            private static Node? removeDuplicates(Node? head)
+            {
+                //Write your code here
+                if (head is null) return head;
+
+                Node? node = head;
+                while (node.next is not null)
+                {
+                    if (node.data == node.next.data)
+                        node.next = node.next.next;
+                    else
+                        node = node.next;
+                }
+
+                return head;
+            }
+
+            private static Node insert(Node? head, int data)
+            {
+                Node p = new Node(data);
+
+
+                if (head == null)
+                    head = p;
+                else if (head.next == null)
+                    head.next = p;
+                else
+                {
+                    Node start = head;
+                    while (start.next != null)
+                        start = start.next;
+                    start.next = p;
+
+                }
+                return head;
+            }
+            private static void display(Node? head)
+            {
+                Node? start = head;
+                while (start != null)
+                {
+                    Console.Write(start.data + " ");
+                    start = start.next;
+                }
+            }
+
+            public static void Main(String[] args)
+            {
+
+                Node? head = null;
+                int T = Int32.Parse(Console.ReadLine());
+                while (T-- > 0)
+                {
+                    int data = Int32.Parse(Console.ReadLine());
+                    head = insert(head, data);
+                }
+                head = removeDuplicates(head);
+                display(head);
+            }
+        }
+    }
+}
