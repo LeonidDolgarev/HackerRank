b9fc456181a7018ee51b970b18bc8003bd3d2125
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-18 08:19:28 +0200

    30 Days of Code: Day 23: BST Level-Order Traversal

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayTwentyThreeBstLevelOrderTraversalTests.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayTwentyThreeBstLevelOrderTraversalTests
+    {
+        [Theory]
+        [InlineData("6\r\n3\r\n5\r\n4\r\n7\r\n2\r\n1", "3 2 5 1 4 7 ")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayTwentyThreeBstLevelOrderTraversal.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayTwentyThreeBstLevelOrderTraversal.cs
@@ -0,0 +1,70 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayTwentyThreeBstLevelOrderTraversal
+    {
+        private sealed class Node
+        {
+            public Node? left, right;
+            public int data;
+            public Node(int data)
+            {
+                this.data = data;
+                left = right = null;
+            }
+        }
+
+        public sealed class Solution
+        {
+            private static void levelOrder(Node? root)
+            {
+                //Write your code here
+                var queue = new Queue<Node?>();
+                queue.Enqueue(root);
+
+                while (queue.Count != 0)
+                {
+                    Node? node = queue.Dequeue();
+                    Console.Write($"{node?.data} ");
+
+                    if (node?.left is not null) queue.Enqueue(node.left);
+                    if (node?.right is not null) queue.Enqueue(node.right);
+                }
+            }
+
+            private static Node insert(Node? root, int data)
+            {
+                if (root == null)
+                {
+                    return new Node(data);
+                }
+                else
+                {
+                    Node cur;
+                    if (data <= root.data)
+                    {
+                        cur = insert(root.left, data);
+                        root.left = cur;
+                    }
+                    else
+                    {
+                        cur = insert(root.right, data);
+                        root.right = cur;
+                    }
+                    return root;
+                }
+            }
+
+            public static void Main(String[] args)
+            {
+                Node? root = null;
+                int T = Int32.Parse(Console.ReadLine());
+                while (T-- > 0)
+                {
+                    int data = Int32.Parse(Console.ReadLine());
+                    root = insert(root, data);
+                }
+                levelOrder(root);
+            }
+        }
+    }
+}
