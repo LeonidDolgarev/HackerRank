2c4a8e01ad79b8b21fbb2ef3af91850c994ab69c
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-20 02:17:37 +0200

    30 Days of Code: Day 28: RegEx, Patterns, and Intro to Databases

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayTwentyEightRegExPatternsAndIntroToDatabasesTests.cs
@@ -0,0 +1,25 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayTwentyEightRegExPatternsAndIntroToDatabasesTests
+    {
+        [Theory]
+        [InlineData("6\r\nriya riya@gmail.com\r\njulia julia@julia.me\r\njulia sjulia@gmail.com\r\njulia julia@gmail.com\r\nsamantha samantha@gmail.com\r\ntanya tanya@gmail.com", "julia\r\njulia\r\nriya\r\nsamantha\r\ntanya\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayTwentyEightRegExPatternsAndIntroToDatabases.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayTwentyEightRegExPatternsAndIntroToDatabases.cs
@@ -0,0 +1,28 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayTwentyEightRegExPatternsAndIntroToDatabases
+    {
+        public sealed class Solution
+        {
+            public static void Main(string[] args)
+            {
+                int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+                var names = new List<string>();
+                for (int NItr = 0; NItr < N; NItr++)
+                {
+                    string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+                    string firstName = firstMultipleInput[0];
+
+                    string emailID = firstMultipleInput[1];
+
+                    if (emailID.EndsWith("@gmail.com")) names.Add(firstName);
+                }
+
+                names.Sort();
+                names.ForEach(x => Console.WriteLine(x));
+            }
+        }
+    }
+}
