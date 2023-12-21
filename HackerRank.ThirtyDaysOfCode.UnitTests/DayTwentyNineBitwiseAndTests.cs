b46b1ad879611d1b246981b5dc8110890b72e12b
Leonid Dolgarev <leonid.dolgarev@gmail.com>
2023-12-21 03:32:35 +0200

    30 Days of Code: Day 29: Bitwise AND

---
+++ HackerRank.ThirtyDaysOfCode.UnitTests/DayTwentyNineBitwiseAndTests.cs
@@ -0,0 +1,26 @@
+﻿namespace HackerRank.ThirtyDaysOfCode.UnitTests
+{
+    public class DayTwentyNineBitwiseAndTests
+    {
+        [Theory]
+        [InlineData("3\r\n5 2\r\n8 5\r\n2 2", "1\r\n4\r\n0\r\n")]
+        [InlineData("2\r\n9 2\r\n8 3", "1\r\n2\r\n")]
+        public void Solution_Main_ShouldPrintResult_WhenInputValues(string input, string expected)
+        {
+            // Arrange
+            StringReader reader = new(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new();
+            Console.SetOut(writer);
+
+            string[] args = { };
+
+            // Act
+            DayTwentyNineBitwiseAnd.Solution.Main(args);
+
+            // Assert
+            Assert.Equal(expected, writer.ToString());
+        }
+    }
+}
---
+++ HackerRank.ThirtyDaysOfCode/DayTwentyNineBitwiseAnd.cs
@@ -0,0 +1,42 @@
+﻿namespace HackerRank.ThirtyDaysOfCode
+{
+    public sealed class DayTwentyNineBitwiseAnd
+    {
+        public sealed class Result
+        {
+            /*
+             * Complete the 'bitwiseAnd' function below.
+             *
+             * The function is expected to return an INTEGER.
+             * The function accepts following parameters:
+             *  1. INTEGER N
+             *  2. INTEGER K
+             */
+            public static int bitwiseAnd(int N, int K)
+            {
+                return ((K | (K - 1)) > N) ? K - 2 : K - 1;
+            }
+        }
+
+        public sealed class Solution
+        {
+            public static void Main(string[] args)
+            {
+                int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+                for (int tItr = 0; tItr < t; tItr++)
+                {
+                    string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+                    int count = Convert.ToInt32(firstMultipleInput[0]);
+
+                    int lim = Convert.ToInt32(firstMultipleInput[1]);
+
+                    int res = Result.bitwiseAnd(count, lim);
+
+                    Console.WriteLine(res);
+                }
+            }
+        }
+    }
+}
